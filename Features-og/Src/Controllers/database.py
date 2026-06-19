0e03435bf306a3950f02db1197dd278b875ed21a
Mariaj_Rojas086 <mariarojasparra928@gmail.com>
2026-06-18 20:23:56 -0500

    Create database.py
    conexión a la base de datos

---
+++ Features-og/Src/Controllers/database.py
@@ -0,0 +1,15 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+def obtener_conexion():
+    try:
+        return psycopg2.connect(
+            host="localhost",
+            database="lynko",         
+            user="postgres",          
+            password="1234",        
+            port="5432"
+        )
+    except Exception as e:
+        print(f"❌ Error crítico al conectar con PostgreSQL: {e}")
+        return None
